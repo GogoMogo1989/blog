0d98744eb8056e8bf2cdee005b524a0effee0c13
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-07 19:10:41 +0200

    20221007

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>David and Mephisto the cat Blog</h1>
+      <h1>David and Mephisto the Cat Blogs</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
